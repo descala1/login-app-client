89d6d36ea7ac7b24e4d134185ca1d8845b00b7b0
Daniel Escalante <daniel.esc11@gmail.com>
2020-08-05 09:40:40 -0400

    production build.

---
+++ build/precache-manifest.270b08a5141520b560f42409b77a5dd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a33b9cf2c0dcf87a9a24699685deb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "197cf4b3fa10fab9a4de",
+    "url": "/static/css/2.a25ff130.chunk.css"
+  },
+  {
+    "revision": "f6bcb8a48e2ad01db60b",
+    "url": "/static/css/main.aa18b413.chunk.css"
+  },
+  {
+    "revision": "197cf4b3fa10fab9a4de",
+    "url": "/static/js/2.87c1ebb8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.87c1ebb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6bcb8a48e2ad01db60b",
+    "url": "/static/js/main.955a240f.chunk.js"
+  },
+  {
+    "revision": "399866405d62b71c4c4d",
+    "url": "/static/js/runtime-main.3a923700.js"
+  }
+]);
